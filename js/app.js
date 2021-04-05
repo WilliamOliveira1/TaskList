2c70aa11d8aaa71b8df42987e372094abc376a50
William Oliveira <1704639@aluno.univesp.br>
2021-04-05 17:19:21 -0300

    set to remove single-task/all-taks and filter it

--- js/app.js
+++ js/app.js
@@ -11,7 +11,13 @@ loadEventListeners();
 //Method to load event listener
 function loadEventListeners() {
     // Add task event
-    form.addEventListener('submit', addTask)
+    form.addEventListener('submit', addTask);
+    //Remove TaskEvent
+    taskList.addEventListener('click', removeTask);
+    //Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter task event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
@@ -33,7 +39,7 @@ function addTask(e) {
         link.className = 'delete-item secondary-content';
 
         //Add icon to link
-        link.innerHTML = '<i class"fa fa-remove"></i>'
+        link.innerHTML = '<i class="fas fa-minus-circle"></i>'
 
         //append link to li
         li.appendChild(link);
@@ -46,4 +52,37 @@ function addTask(e) {
     }
 
     e.preventDefault();
+}
+
+
+//Method to remove single task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure that you want to delete this task?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Method to remove all tasks
+function clearTasks() {
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild)
+    }
+}
+
+// Method to filter task 
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    
+    const filter = document.querySelectorAll('.collection-item');
+
+    filter.forEach(function(task) {
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block'
+        }else {
+            task.style.display = 'none'
+        }
+    })
 }
